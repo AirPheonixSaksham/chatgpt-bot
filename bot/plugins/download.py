7de04116bb0fd1c276070b65539c03f677f40542
AirPheonixSaksham <166398246+AirPheonixSaksham@users.noreply.github.com>
2025-01-23 01:14:36 +0530

    Update download.py

--- bot/plugins/download.py
+++ bot/plugins/download.py
@@ -172,7 +172,7 @@ async def download(bot: Client, message: types.Message, start_index=0, end_index
                     "width": width,
                     "height": height,
                     "chat_id": user["log_channel"] or message.chat.id,
-                    }
+            }
         else:
              func = bot.send_document
              kwargs = {
